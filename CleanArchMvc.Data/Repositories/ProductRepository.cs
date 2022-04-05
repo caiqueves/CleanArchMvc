7a47aa35e0f093b34da4fed831648f18ae742f7e
caique silva <caique_neves@outlook.com.br>
2022-04-04 22:30:44 -0300

    Implementação parcial do CQRS

--- CleanArchMvc.Data/Repositories/ProductRepository.cs
+++ CleanArchMvc.Data/Repositories/ProductRepository.cs
@@ -24,7 +24,9 @@ namespace CleanArchMvc.Infra.Data.Repositories
 
         public async Task<Product> GetByIdAsync(int? id)
         {
-            return await _productContext.Products.FindAsync(id);
+            //return await _productContext.Products.FindAsync(id);
+            return await _productContext.Products.Include(c => c.Category)
+                .SingleOrDefaultAsync(p => p.Id == id);
         }
 
         public async Task<IEnumerable<Product>> GetProductAsync()
@@ -32,12 +34,12 @@ namespace CleanArchMvc.Infra.Data.Repositories
             return await _productContext.Products.ToListAsync();
         }
 
-        public async Task<Product> GetProductCategoryAsync(int? id)
-        {
-            //eager loading
-            return await _productContext.Products.Include(c => c.Category)
-                .SingleOrDefaultAsync(p => p.Id == id);
-        }
+        //public async Task<Product> GetProductCategoryAsync(int? id)
+        //{
+        //    //eager loading
+        //    return await _productContext.Products.Include(c => c.Category)
+        //        .SingleOrDefaultAsync(p => p.Id == id);
+        //}
 
         public async Task<IEnumerable<Product>> GetProductsAsync()
         {
--- CleanArchMvc.Infra.Ioc/DependencyInjection.cs
+++ CleanArchMvc.Infra.Ioc/DependencyInjection.cs
@@ -4,6 +4,7 @@ using CleanArchMvc.Application.Services;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
 using CleanArchMvc.Infra.Data.Repositories;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,6 +25,9 @@ namespace CleanArchMvc.Infra.IoC
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myhandlers = System.AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+            services.AddMediatR(myhandlers);
             return services;
 
         }
